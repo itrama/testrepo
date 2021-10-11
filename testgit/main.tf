81955d177faf690589e4c3e0eb63f9a54721ceb0
unknown <chandra151194@gmail.com>
2021-10-11 13:14:16 +0000

    p first commit

--- testgit/main.tf
+++ testgit/main.tf
@@ -34,6 +34,17 @@ resource "aws_internet_gateway" "default" {
     }
 }
 
+
+resource "aws_subnet" "subnet2-public" {
+    vpc_id = "${aws_vpc.default.id}"
+    cidr_block = "${var.public_subnet1_cidr}"
+    availability_zone = "us-east-1a"
+
+    tags = {
+        Name = "${var.public_subnet1_name}"
+    }
+}
+
 resource "aws_subnet" "subnet2-public" {
     vpc_id = "${aws_vpc.default.id}"
     cidr_block = "${var.public_subnet2_cidr}"
